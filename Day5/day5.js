95a2e79bdfcf167ae8a49f9add872b25cb7ad471
sulmeee <poorooria@naver.com>
2022-11-23 14:31:50 +0900

    docs: Day 5

--- Day5/day5.js
+++ Day5/day5.js
@@ -7,16 +7,12 @@ calculate.addEventListener('click', () => {
     let userYear = inputDate.getFullYear();
     let userMonth = inputDate.getMonth() + 1;
     let userDay = inputDate.getDate();
-    console.log(userYear, userMonth, userDay)
-
 
     // current date
     let currentDate = new Date();
     let currentYear = currentDate.getFullYear();
     let currentMonth = currentDate.getMonth() + 1;
     let currentDay = currentDate.getDate();
-    console.log(currentYear, currentMonth, currentDay)
-    console.log(currentDate)
 
     // get year
     let year = currentYear - userYear;
--- Day5/day5.md
+++ Day5/day5.md
@@ -1,4 +1,15 @@
 ## Day 5 Age Calculator
 
 </br>
-몰랐던 개념
+
+### new Date()
+
+</br>
+
+new Date 함수는 현재 시간을 알려준다
+
+getDay 대신 getDate를 사용해서 일수 구하기
+
+Day를 사용하면 요일을 반환해서 0~6을 출력한다
+
+Date를 사용하면 해당 일을 구할 수 있다
